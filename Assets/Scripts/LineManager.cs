61aa2bd33d29a0bb2207a155d03bd79e7e4026ed
nature <2726916340@qq.com>
2018-11-02 16:24:18 +0800

    Gerometric Mathematics Simulator

---
+++ Assets/Scripts/LineManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineManager : MonoBehaviour {
+
+	public Vector3 StartPoint;
+	public Vector3 EndPoint;
+
+	public Vector2 GetVector()
+	{
+		float x = StartPoint.x - EndPoint.x;
+		float y = StartPoint.y - EndPoint.y;
+		return new Vector2(x,y);
+	}
+}
